ac5473777a4ee95d9d7c39c3c194a6319b1c1e9c
Matthias Günther <matgnt@gmail.com>
2015-05-23 20:50:31 +0200

    Add "Quantity" class

--- lib/epcisevents.ts
+++ lib/epcisevents.ts
@@ -94,4 +94,54 @@ export module EPCIS {
 		id: string;
 	}
 
+	export class Quantity {
+		/*
+		* GTIN: 			urn:epc:idpat:sgtin:...
+		* GTIN + batch/lot:	urn:epc:class:lgtin:...
+		* GTIN + serial:	urn:epc:id:sgtin:...
+		* SSCC:				urn:epc:id:sscc:...
+		* GRAI (no serial):	urn:epc:idpat:grai:...
+		* GRAI (serial):	urn:epc:id:grai:...
+		*
+		* and many more. Check the specification.
+		* The prefix describes the value.
+		*/
+		//epcClass: string;
+		// we split the "epcClass" field into the following 2
+		type: string;
+		identifier: string;
+
+		quantity: number;
+
+		/*
+		* "uom" but who the heck knows what that means?
+		* Not needed for just a quantity, but as soon as a unit is needed,
+		* it should be the 2-3 character code for the physical unit
+		* specified in the "Common Code" of "UN/CEFACT"
+		*
+		* Examples:
+		* kg: KGM
+		* liter: LTR
+		* meter: MTR
+		*/
+		unit: string;
+
+		// convenient function to get the 2 fields together (as it is
+		// described in the EPCIS standard)
+		epcClass(): string {
+			return this.type + ':' + this.identifier;
+		}
+
+		// Convenient function to parse the EPCIS standard field
+		// and split it into the two fields "type" and "identifier".
+		// Split at the last ":"
+		setEpcClass(epcClass: string): void {
+			var pos:number = epcClass.lastIndexOf(':');
+			if(pos > 0) {
+				this.type = epcClass.substr(0, pos);
+				this.identifier = epcClass.substr(pos+1);
+			}
+		}
+	}
+
 }
